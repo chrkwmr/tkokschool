2792a374c4bc3ec61c61f6f6031873912c7ce276
chihiro kawamura <kawamura@ediplex.co.jp>
2015-05-27 16:13:12 +0900

    モーダル編集
    しました

--- js/script.js
+++ js/script.js
@@ -24,5 +24,17 @@ $(function(){
       $(this).find('.gallery-cover').fadeOut();
     }
   );
+
+  $('#contact-show').click(function(){
+    $('#modal-contact').hide();
+  });
+  //「閉じる」ボタンが押されたときに
+  //「#modal-contact」を隠してください
+
+  $('#submit-btn').click(function(){
+    $('#contact-form').html(<div class="thanks-msg">お問い合わせいただきありがとうございます。<br>内容が確認でき次第返信をさせていただきますので、<br>今しばらくお待ち下さい。</div>);
+  });
+  //「送信」ボタンが押されたときに
+  //「#contact-form」の中身を書きかえてください
 	
 });
